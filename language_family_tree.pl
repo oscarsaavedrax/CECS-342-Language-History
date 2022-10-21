431e3f9a2f237fdac60153133a3c3aea434647d2
oscarsaavedrax <103982163+oscarsaavedrax@users.noreply.github.com>
2022-10-20 19:51:49 -0700

    Add parent facts

--- language_family_tree.pl
+++ language_family_tree.pl
@@ -39,8 +39,71 @@ born(csharp2,2004).
 born(go,2008).
 born(kotlin,2010).
 born(rust,2010).
-born(haskell,2010).
+born(haskell2010,2010).
 born(csharp5,2012).
 born(java8,2014).
 born(swift,2014).
 born(javascriptes2017,2017).
+
+%parent(language, language).
+parent(fortran1, algo60).
+parent(fortran1, pl1).
+parent(fortran1, fortran77).
+parent(algo60, tcl).
+parent(algo60, cpp).
+parent(algo60, c).
+parent(algo60, smalltalk).
+parent(algo60, perl).
+parent(algo60, scheme).
+parent(algo60, pl1).
+parent(cobol, pl1).
+parent(lisp, scheme).
+parent(lisp, commonlisp).
+parent(cpp, java).
+parent(cpp, cppiso).
+parent(cpp, rust).
+parent(cpp, python).
+parent(c, cpp).
+parent(c, python).
+parent(smalltalk, smalltalk80).
+parent(perl, ruby).
+parent(perl, perl5).
+parent(pl1, pascal).
+parent(scheme, schemer5rs).
+parent(scheme, commonlisp).
+parent(fortran77, fortran90).
+parent(java, java5).
+parent(java, csharp).
+parent(java, javascript).
+parent(cppiso, csharp).
+parent(rust, swift).
+parent(python, python2).
+parent(python, ruby).
+parent(smalltalk80, go).
+parent(smalltalk80, ruby).
+parent(pascal, python).
+parent(pascal, ada83).
+parent(ml, haskell98).
+parent(ml, sml).
+parent(java5, java8).
+parent(java5, kotlin).
+parent(csharp, csharp2).
+parent(javascript, kotlin).
+parent(javascript, javascriptes2017).
+parent(ada83, eiffel).
+parent(eiffel, ruby).
+parent(sml, caml).
+parent(caml, ocaml).
+parent(ocaml, rust).
+parent(csharp2, kotlin).
+parent(csharp2, csharp5).
+parent(csharp2, rust).
+parent(csharp5, javascriptes2017).
+parent(csharp5, swift).
+parent(python2, swift).
+parent(python2, go).
+parent(ruby, swift).
+parent(schemer5rs, rust).
+parent(haskell98, rust).
+parent(haskell98, haskell2010).
+parent(haskell2010, swift).
